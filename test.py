e6f850cf39fe174aed7cd4bf1c29bd50c5441467
timmy <timmy.chiu@beeyoung.tech>
2025-07-09 12:07:34 +0800

    更新 鍵盤測試改為20秒沒按變紅背景、改為下壓觸發、紅色背景改深

--- test.py
+++ test.py
@@ -1,13 +1,19 @@
-import subprocess
+import ctypes
 import time
-import pygetwindow as gw
 
-# 使用 subprocess 啟動 paint.exe
-# subprocess.Popen(["dist/paint.exe"])  # 確保路徑正確
-#
-# time.sleep(7)
+# 虛擬鍵碼定義
+VK_NUMLOCK = 0x90
+KEYEVENTF_EXTENDEDKEY = 0x0001
+KEYEVENTF_KEYUP = 0x0002
 
-window = gw.getWindowsWithTitle('touch test')
-print(window)
-if window:
-    window[0].maximize()  # 將小畫家視窗最大化
+# 模擬按下和放開 NumLock 鍵
+def toggle_numlock():
+    # 模擬按鍵
+    ctypes.windll.user32.keybd_event(VK_NUMLOCK, 0x45, KEYEVENTF_EXTENDEDKEY, 0)
+    # 模擬放開
+    ctypes.windll.user32.keybd_event(VK_NUMLOCK, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0)
+
+# 執行範例
+toggle_numlock()
+time.sleep(1)
+toggle_numlock()
